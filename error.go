f621c8211ae096eb0e6d868471179f944d2ba576
Oscar Söderlund <odsod@users.noreply.github.com>
2025-11-13 18:46:43 +0100

    fix: forward HTTP body for response errors
    MBZ API provides additional error context in the body - useful for
    debugging.

--- error.go
+++ error.go
@@ -2,11 +2,33 @@ package mbz
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
-// newResponseError creates a new error from an HTTP response.
+// ResponseError represents a VW Fleet Interface response error.
+type ResponseError struct {
+	// StatusCode is the HTTP status code of the response.
+	StatusCode int
+	// Body is the body of the response.
+	Body []byte
+}
+
 func newResponseError(httpResponse *http.Response) error {
-	// TOFDO: Implement error parsing.
-	return fmt.Errorf("HTTP %s", httpResponse.Status)
+	body, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		body = fmt.Appendf(nil, "failed to read response body: %s", err)
+	}
+	return &ResponseError{
+		StatusCode: httpResponse.StatusCode,
+		Body:       body,
+	}
+}
+
+// Error implements the error interface.
+func (e *ResponseError) Error() string {
+	if len(e.Body) > 0 {
+		return fmt.Sprintf("http %d: %s", e.StatusCode, string(e.Body))
+	}
+	return fmt.Sprintf("http %d", e.StatusCode)
 }
